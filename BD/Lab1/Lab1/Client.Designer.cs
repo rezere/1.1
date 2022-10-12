6e7f7b24a4434e526ab5df00d253a2e639542857
Николай Войцехов <froust87@gmail.com>
2022-10-12 13:54:36 +0300

    12.10

---
+++ BD/Lab1/Lab1/Client.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace Lab1
+{
+    partial class Client
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.hospitalDataSet = new Lab1.hospitalDataSet();
+            this.hospitalDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataSet1 = new Lab1.DataSet1();
+            this.dataSet1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.medicBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.medicTableAdapter = new Lab1.DataSet1TableAdapters.medicTableAdapter();
+            this.clientBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.clientTableAdapter = new Lab1.DataSet1TableAdapters.clientTableAdapter();
+            this.fKappealmedicBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.appealTableAdapter = new Lab1.DataSet1TableAdapters.appealTableAdapter();
+            this.fKappealmedicBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.hospitalDataSetBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.hospitalDataSet1 = new Lab1.hospitalDataSet1();
+            this.medicBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.medicTableAdapter1 = new Lab1.hospitalDataSet1TableAdapters.medicTableAdapter();
+            this.kodDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.surnameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.patronymicDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.specialtyDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.categoryDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.hospitalDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hospitalDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1BindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.medicBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.clientBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fKappealmedicBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fKappealmedicBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hospitalDataSetBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hospitalDataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.medicBindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // hospitalDataSet
+            // 
+            this.hospitalDataSet.DataSetName = "hospitalDataSet";
+            this.hospitalDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // hospitalDataSetBindingSource
+            // 
+            this.hospitalDataSetBindingSource.DataSource = this.hospitalDataSet;
+            this.hospitalDataSetBindingSource.Position = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.kodDataGridViewTextBoxColumn,
+            this.surnameDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn,
+            this.patronymicDataGridViewTextBoxColumn,
+            this.specialtyDataGridViewTextBoxColumn,
+            this.categoryDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.medicBindingSource1;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 321);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(753, 117);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "DataSet1";
+            this.dataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // dataSet1BindingSource
+            // 
+            this.dataSet1BindingSource.DataSource = this.dataSet1;
+            this.dataSet1BindingSource.Position = 0;
+            // 
+            // medicBindingSource
+            // 
+            this.medicBindingSource.DataMember = "medic";
+            this.medicBindingSource.DataSource = this.dataSet1BindingSource;
+            // 
+            // medicTableAdapter
+            // 
+            this.medicTableAdapter.ClearBeforeFill = true;
+            // 
+            // clientBindingSource
+            // 
+            this.clientBindingSource.DataMember = "client";
+            this.clientBindingSource.DataSource = this.dataSet1BindingSource;
+            // 
+            // clientTableAdapter
+            // 
+            this.clientTableAdapter.ClearBeforeFill = true;
+            // 
+            // fKappealmedicBindingSource
+            // 
+            this.fKappealmedicBindingSource.DataMember = "FK_appeal_medic";
+            this.fKappealmedicBindingSource.DataSource = this.medicBindingSource;
+            // 
+            // appealTableAdapter
+            // 
+            this.appealTableAdapter.ClearBeforeFill = true;
+            // 
+            // fKappealmedicBindingSource1
+            // 
+            this.fKappealmedicBindingSource1.DataMember = "FK_appeal_medic";
+            this.fKappealmedicBindingSource1.DataSource = this.medicBindingSource;
+            // 
+            // hospitalDataSetBindingSource1
+            // 
+            this.hospitalDataSetBindingSource1.DataSource = this.hospitalDataSet;
+            this.hospitalDataSetBindingSource1.Position = 0;
+            // 
+            // hospitalDataSet1
+            // 
+            this.hospitalDataSet1.DataSetName = "hospitalDataSet1";
+            this.hospitalDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // medicBindingSource1
+            // 
+            this.medicBindingSource1.DataMember = "medic";
+            this.medicBindingSource1.DataSource = this.hospitalDataSet1;
+            // 
+            // medicTableAdapter1
+            // 
+            this.medicTableAdapter1.ClearBeforeFill = true;
+            // 
+            // kodDataGridViewTextBoxColumn
+            // 
+            this.kodDataGridViewTextBoxColumn.DataPropertyName = "kod";
+            this.kodDataGridViewTextBoxColumn.HeaderText = "kod";
+            this.kodDataGridViewTextBoxColumn.Name = "kodDataGridViewTextBoxColumn";
+            this.kodDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // surnameDataGridViewTextBoxColumn
+            // 
+            this.surnameDataGridViewTextBoxColumn.DataPropertyName = "surname";
+            this.surnameDataGridViewTextBoxColumn.HeaderText = "surname";
+            this.surnameDataGridViewTextBoxColumn.Name = "surnameDataGridViewTextBoxColumn";
+            this.surnameDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "name";
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            this.nameDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // patronymicDataGridViewTextBoxColumn
+            // 
+            this.patronymicDataGridViewTextBoxColumn.DataPropertyName = "patronymic";
+            this.patronymicDataGridViewTextBoxColumn.HeaderText = "patronymic";
+            this.patronymicDataGridViewTextBoxColumn.Name = "patronymicDataGridViewTextBoxColumn";
+            this.patronymicDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // specialtyDataGridViewTextBoxColumn
+            // 
+            this.specialtyDataGridViewTextBoxColumn.DataPropertyName = "specialty";
+            this.specialtyDataGridViewTextBoxColumn.HeaderText = "specialty";
+            this.specialtyDataGridViewTextBoxColumn.Name = "specialtyDataGridViewTextBoxColumn";
+            this.specialtyDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // categoryDataGridViewTextBoxColumn
+            // 
+            this.categoryDataGridViewTextBoxColumn.DataPropertyName = "category";
+            this.categoryDataGridViewTextBoxColumn.HeaderText = "category";
+            this.categoryDataGridViewTextBoxColumn.Name = "categoryDataGridViewTextBoxColumn";
+            this.categoryDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // Client
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Client";
+            this.Text = "Client";
+            this.Load += new System.EventHandler(this.Client_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.hospitalDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hospitalDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1BindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.medicBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.clientBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fKappealmedicBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fKappealmedicBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hospitalDataSetBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hospitalDataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.medicBindingSource1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.BindingSource hospitalDataSetBindingSource;
+        private hospitalDataSet hospitalDataSet;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource dataSet1BindingSource;
+        private DataSet1 dataSet1;
+        private System.Windows.Forms.BindingSource medicBindingSource;
+        private DataSet1TableAdapters.medicTableAdapter medicTableAdapter;
+        private System.Windows.Forms.BindingSource clientBindingSource;
+        private DataSet1TableAdapters.clientTableAdapter clientTableAdapter;
+        private System.Windows.Forms.BindingSource fKappealmedicBindingSource;
+        private DataSet1TableAdapters.appealTableAdapter appealTableAdapter;
+        private System.Windows.Forms.BindingSource hospitalDataSetBindingSource1;
+        private System.Windows.Forms.BindingSource fKappealmedicBindingSource1;
+        private hospitalDataSet1 hospitalDataSet1;
+        private System.Windows.Forms.BindingSource medicBindingSource1;
+        private hospitalDataSet1TableAdapters.medicTableAdapter medicTableAdapter1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn kodDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn surnameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn patronymicDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn specialtyDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn categoryDataGridViewTextBoxColumn;
+    }
+}
---
+++ BD/Lab1/Lab1/Client.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+    public partial class Client : Form
+    {
+        public Client()
+        {
+            InitializeComponent();
+        }
+
+        private void Client_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "hospitalDataSet1.medic". При необходимости она может быть перемещена или удалена.
+            this.medicTableAdapter1.Fill(this.hospitalDataSet1.medic);
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "dataSet1.appeal". При необходимости она может быть перемещена или удалена.
+            this.appealTableAdapter.Fill(this.dataSet1.appeal);
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "dataSet1.client". При необходимости она может быть перемещена или удалена.
+            this.clientTableAdapter.Fill(this.dataSet1.client);
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "dataSet1.medic". При необходимости она может быть перемещена или удалена.
+            this.medicTableAdapter.Fill(this.dataSet1.medic);
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
